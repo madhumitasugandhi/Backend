89c87a191c4a7577dca700c8b4fb151fe4cdd862
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-05-21 15:10:56 +0530

    Inbuilt and custom middleware added, used postman.

--- index.js
+++ index.js
@@ -2,7 +2,31 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json());
+
+const loggingMiddleware = function (req, res, next){
+    console.log('LOGING')
+    next ();
+}
+
+app.use(loggingMiddleware);
+
+const authMiddleware = function (req, res, next) {
+    console.log ('Authentication')
+    next();
+};
+
+app.use(authMiddleware);
+
+const validationMiddleware = function (req, res, next){
+    console.log('Validation')
+    next ();
+}
+app.use(validationMiddleware);
+
 app.get('/', (req, res) => {
+    console.log("Madhumita")
+    console.log(req.body);
   res.send('Hello World!')
 })
 
